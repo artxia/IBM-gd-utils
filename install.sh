b706ee24dac5bed66c5a0843258f6c0d60bc01bd
xia <50513568+artxia@users.noreply.github.com>
2020-08-15 18:55:04 +0800

    add install

---
+++ install.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+SH_PATH=$(cd "$(dirname "$0")";pwd)
+cd ${SH_PATH}
+
+create_mainfest_file(){
+    echo "进行配置。。。"
+    read -p "请输入你的应用名称：" IBM_APP_NAME
+    echo "应用名称：${IBM_APP_NAME}"
+    read -p "请输入你的应用内存大小(默认256)：" IBM_MEM_SIZE
+    if [ -z "${IBM_MEM_SIZE}" ];then
+    IBM_MEM_SIZE=256
+    fi
+    echo "内存大小：${IBM_MEM_SIZE}"
+    read -p "请输入你的应用区域：" IBM_APP_REGION
+    echo "应用区域：${IBM_APP_REGION}"
+
+    read -p """请输入机器人token：" YOUR_BOT_TOKEN
+while [[ "${#YOUR_BOT_TOKEN}" != 46 ]]; do
+    echo "机器人TOKEN输入不正确，请重新输入"
+    read -p """请输入机器人token：" YOUR_BOT_TOKEN
+done
+
+read -p "请输入使用机器人的telegram账号ID：" YOUR_TELEGRAM_ID
+echo "你的TG账号${YOUR_TELEGRAM_ID}"
+
+read -p "请输入转存默认目的地团队盘ID：" YOUR_GOOGLE_TEAM_DRIVE_ID
+while [[ "${#YOUR_GOOGLE_TEAM_DRIVE_ID}" != 19 ]]; do
+    echo "你的Google team drive ID输入不正确"
+    read -p "请输入转存默认目的地ID：" YOUR_GOOGLE_TEAM_DRIVE_ID
+done
+
+read -p "请输入你的IBM应用URL：" IBM_APP_URL
+echo "你的IBM应用URL${IBM_APP_URL}"
+
+cd ~ &&
+    sed -i "s/cloud_fonudray_name/${IBM_APP_NAME}/g" ${SH_PATH}/IBM-gd-utils/manifest.yml &&
+    sed -i "s/cloud_fonudray_mem/${IBM_MEM_SIZE}/g" ${SH_PATH}/IBM-gd-utils/manifest.yml && 
+    sed -i "s/bot_token/${YOUR_BOT_TOKEN}/g" ${SH_PATH}/IBM-gd-utils/gd-utils/config.js &&
+    sed -i "s/your_tg_username/${YOUR_TELEGRAM_ID}/g" ${SH_PATH}/IBM-gd-utils/gd-utils/config.js && 
+    sed -i "s/DEFAULT_TARGET = ''/DEFAULT_TARGET = '${YOUR_GOOGLE_TEAM_DRIVE_ID}'/g" ${SH_PATH}/IBM-gd-utils/gd-utils/config.js&&
+    sed -i "s/23333/8080/g" ${SH_PATH}/IBM-gd-utils/gd-utils/server.js &&
+    sed -i "s/"start": "https_proxy='http://127.0.0.1:1086' nodemon server.js"/"start": "nodemon server.js"/g" ${SH_PATH}/IBM-gd-utils/gd-utils/package.json&&
+    sed -i '/"repository": "iwestlin/gd-utils",/a\"engines": {\n  "node": "12.*"\n},' ${SH_PATH}/IBM-gd-utils/gd-utils/package.json
+    echo "配置完成。"
+}
+
+clone_repo(){
+    echo "进行初始化。。。"
+    git clone https://github.com/artxia/IBM-gd-utils
+cd IBM-gd-utils
+    git submodule update --init --recursive
+cd gd-utils
+    npm i
+cd sa
+    echo "请点击网页右上角的上传功能，上传sa打包的accounts.zip文件，注意命名和压缩格式要相同"
+if test -s ${SH_PATH}/accounts.zip; then
+    echo "正在解压。。。"
+    cp -r ${SH_PATH}/accounts.zip /IBM-gd-utils/gd-utils/sa/
+    unzip -j accounts.zip -y
+    sleep 10s
+    echo "初始化完成。"
+}
+
+install(){
+    echo "进行安装。。。"
+    cd ${SH_PATH}/IBM-gd-utils
+    ibmcloud target --cf
+    ibmcloud cf install
+    ibmcloud cf push
+    echo "安装完成。"
+    sleep 3s
+	echo "检查是否部署成功。。。"
+    echo ${IBM_APP_NAME}.${IBM_APP_REGION}.cf.appdomain.cloud/api/gdurl/count?fid=124pjM5LggSuwI1n40bcD5tQ13wS0M6wg
+    curl ${IBM_APP_NAME}.${IBM_APP_REGION}.cf.appdomain.cloud/api/gdurl/count?fid=124pjM5LggSuwI1n40bcD5tQ13wS0M6wg
+    echo
+}
+
+clone_repo
+create_mainfest_file
+install
+exit 0
